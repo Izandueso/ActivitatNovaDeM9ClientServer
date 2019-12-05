6056d4fab913027eb5fd10692a1f86c24c90d071
Alumne <Alumne@104-PC12>
2019-12-05 16:16:39 +0100

    Commit2

--- src/ServidorTCP2.java
+++ src/ServidorTCP2.java
@@ -8,30 +8,28 @@ public class ServidorTCP2 {
 		int numPort = 60000;
 		ServerSocket servidor = new ServerSocket(numPort);
 		String cadena = "";
-		String cadena2 = "";
-		String cadena3 = "";
 		
-		System.out.println("Esperant connexió al client 1... ");
-		Socket clientConnectat = servidor.accept();
-		System.out.println("Client 1 connectat... ");
+		Socket clientConnectat = null;
 		
-		System.out.println("Esperant connexió al client 2... ");
-		Socket clientConnectat2 = servidor.accept();
-		System.out.println("Client 2 connectat... ");
+		for (int i = 0; i < 3; i++) {
+		   	 System.out.println("Esperant connexió... ");
+			 clientConnectat = servidor.accept();
+			 System.out.println("Client connectat... ");
+		}
 		
-		System.out.println("Esperant connexió al client 3... ");
-		Socket clientConnectat3 = servidor.accept();
-		System.out.println("Client 3 connectat... ");
+		PrintWriter fsortida = null;
 		
-		//FLUX DE SORTIDA AL CLIENT
-		PrintWriter fsortida = new PrintWriter(clientConnectat.getOutputStream(), true);
-		PrintWriter fsortida2 = new PrintWriter(clientConnectat2.getOutputStream(), true);
-		PrintWriter fsortida3 = new PrintWriter(clientConnectat3.getOutputStream(), true);
+		for (int i = 0; i < 3; i++) {
+			//FLUX DE SORTIDA AL CLIENT
+			 fsortida = new PrintWriter(clientConnectat.getOutputStream(), true);
+		}
+	
+		BufferedReader fentrada = null;
 		
-		//FLUX D'ENTRADA DEL CLIENT
-		BufferedReader fentrada = new BufferedReader(new InputStreamReader(clientConnectat.getInputStream()));
-		BufferedReader fentrada2 = new BufferedReader(new InputStreamReader(clientConnectat2.getInputStream()));
-		BufferedReader fentrada3 = new BufferedReader(new InputStreamReader(clientConnectat3.getInputStream()));
+		for (int i = 0; i < 3; i++) {
+			//FLUX D'ENTRADA DEL CLIENT
+			 fentrada = new BufferedReader(new InputStreamReader(clientConnectat.getInputStream()));
+		}
 		
 		while ((cadena = fentrada.readLine()) != null) {
 			
@@ -41,36 +39,13 @@ public class ServidorTCP2 {
 			
 		}
 		
-		while ((cadena2 = fentrada2.readLine()) != null) {
-			
-			fsortida2.println(cadena2);
-			System.out.println("Rebent: "+cadena2);
-			if (cadena2.equals("*")) break;
-			
-		}
-
-		while ((cadena3 = fentrada3.readLine()) != null) {
-	
-				fsortida3.println(cadena3);
-				System.out.println("Rebent: "+cadena3);
-				if (cadena3.equals("*")) break;
-	
-		}
-		
 		//TANCAR STREAMS I SOCKETS
 		System.out.println("Tancant connexió... ");
 		fentrada.close();
-		fentrada2.close();
-		fentrada3.close();
 		fsortida.close();
-		fsortida2.close();
-		fsortida3.close();
 		clientConnectat.close();
-		clientConnectat2.close();
-		clientConnectat3.close();
 		servidor.close();
 		
 	}
 
 }
-
