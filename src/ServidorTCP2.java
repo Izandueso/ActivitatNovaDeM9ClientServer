7f5731eeb25b3a65de273eaa71a50b46df33e48d
Alumne <Alumne@104-PC12>
2019-12-05 16:28:52 +0100

    CommitFinal

--- src/ServidorTCP2.java
+++ src/ServidorTCP2.java
@@ -1,17 +1,21 @@
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class ServidorTCP2 {
 	
 	public static void main (String[] args) throws Exception {
-		
+		Scanner teclado = new Scanner(System.in);
 		int numPort = 60000;
 		ServerSocket servidor = new ServerSocket(numPort);
 		String cadena = "";
 		
 		Socket clientConnectat = null;
 		
-		for (int i = 0; i < 3; i++) {
+		System.out.println("Introdueix el numero de usuaris que vols introduir: ");
+		int numusuaris = teclado.nextInt();
+		
+		for (int i = 0; i < numusuaris; i++) {
 		   	 System.out.println("Esperant connexió... ");
 			 clientConnectat = servidor.accept();
 			 System.out.println("Client connectat... ");
@@ -19,14 +23,14 @@ public class ServidorTCP2 {
 		
 		PrintWriter fsortida = null;
 		
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < numusuaris; i++) {
 			//FLUX DE SORTIDA AL CLIENT
 			 fsortida = new PrintWriter(clientConnectat.getOutputStream(), true);
 		}
 	
 		BufferedReader fentrada = null;
 		
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < numusuaris; i++) {
 			//FLUX D'ENTRADA DEL CLIENT
 			 fentrada = new BufferedReader(new InputStreamReader(clientConnectat.getInputStream()));
 		}
